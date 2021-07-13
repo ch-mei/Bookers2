32caffe7b6945ac513c23ad8c2571b2864cae444
ch-mei <mei013h0ws@gmail.com>
2021-07-13 12:28:20 +0000

    0713

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  get 'home/about' => 'homes#about'
+  
+  resources :books, only: [:new, :create, :index, :show, :destroy]do
+  end
+  resources :users, only: [:show]
 end
