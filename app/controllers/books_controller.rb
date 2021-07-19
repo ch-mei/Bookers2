e789ad8a27be45469be3e1137eeedf825a30c21a
ch-mei <mei013h0ws@gmail.com>
2021-07-19 13:31:22 +0000

    0719

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,33 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
     @user = User.find_by(id: @book.user_id)
   end
@@ -24,7 +42,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body)
   end
 
 end
